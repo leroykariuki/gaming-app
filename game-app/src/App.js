baf913a71cc4e8a8043413f6b1acb5263ecffca2
leroy <leroy.kariuki@student.moringaschool.com>
2023-08-02 10:55:55 +0300

    App.js,GameList

---
+++ game-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GamesList from './GamesList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Games</h1>
+      <GamesList />
+    </div>
+  );
+}
+
+export default App;
