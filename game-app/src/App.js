548ad42be33fc76e9593211933078cacc48eecdb
leroy <leroy.kariuki@student.moringaschool.com>
2023-08-03 11:05:46 +0300

    GameList.js

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { fetchGames } from "./Api";
+import CurrentGames from "./CurrentGames";
+import UpcomingGames from "./UpcomingGames";
 
 const App = () => {
   const [games, setGames] = useState({ currentGames: [], upcomingGames: [] });
