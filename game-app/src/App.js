df78718150dd12cf8c0f6755c5608dd774397fad
leroy <leroy.kariuki@student.moringaschool.com>
2023-08-03 16:15:26 +0300

    App.js

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -3,26 +3,45 @@ import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import CurrentGames from "./CurrentGames";
 import UpcomingGames from "./UpcomingGames";
 import MyGames from "./MyGames";
-import SearchBar from "./SearchBar"
+import SearchBar from "./SearchBar";
 
 const App = () => {
   const [currentGames, setCurrentGames] = useState([]);
   const [upcomingGames, setUpcomingGames] = useState([]);
-  const [searchTerm, setSearchTerm] =useState("")
+  const [searchTerm, setSearchTerm] = useState("");
   const [filteredCurrentGames, setFilteredCurrentGames] = useState([]);
   const [filteredUpcomingGames, setFilteredUpcomingGames] = useState([]);
-  
+
   useEffect(() => {
     fetch("http://localhost:3000/currentgames")
       .then((resp) => resp.json())
-      .then((data) => setCurrentGames(data));
+      .then((data) => {
+        setCurrentGames(data);
+        setFilteredCurrentGames(data); 
+      });
   }, []);
 
   useEffect(() => {
     fetch("http://localhost:3000/upcominggames")
       .then((resp) => resp.json())
-      .then((data) => setUpcomingGames(data));
+      .then((data) => {
+        setUpcomingGames(data);
+        setFilteredUpcomingGames(data); 
+      });
   }, []);
+
+  const handleSearchButtonClick = () => {
+    const searchTermLowerCase = searchTerm.toLowerCase();
+    const filteredCurrent = currentGames.filter((game) =>
+      game.title.toLowerCase().includes(searchTermLowerCase)
+    );
+    const filteredUpcoming = upcomingGames.filter((game) =>
+      game.title.toLowerCase().includes(searchTermLowerCase)
+    );
+
+    setFilteredCurrentGames(filteredCurrent);
+    setFilteredUpcomingGames(filteredUpcoming);
+  };
 
   return (
     <Router>
@@ -39,13 +58,26 @@ const App = () => {
               <Link to="/my-games">My Games</Link>
             </li>
           </ul>
-          <SearchBar searchTerm={searchTerm} onSearch={setSearchTerm} />
+          <SearchBar
+            searchTerm={searchTerm}
+            onSearch={setSearchTerm}
+            onSearchButtonClick={handleSearchButtonClick}
+          />
         </nav>
         <hr />
         <Routes>
-          <Route path="/" element={<CurrentGames currentGames={currentGames} />} />
-          <Route path="/upcoming" element={<UpcomingGames upcomingGames={upcomingGames} />} />
-          <Route path="/my-games" element={<MyGames currentGames={currentGames} upcomingGames={upcomingGames} />} />
+          <Route path="/" element={<CurrentGames currentGames={filteredCurrentGames} />} />
+          <Route path="/upcoming" element={<UpcomingGames upcomingGames={filteredUpcomingGames} />} />
+          <Route
+            path="/my-games"
+            element={
+              <MyGames
+                currentGames={currentGames}
+                upcomingGames={upcomingGames}
+                searchTerm={searchTerm}
+              />
+            }
+          />
         </Routes>
       </div>
     </Router>
