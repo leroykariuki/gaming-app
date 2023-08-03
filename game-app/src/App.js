46900fbccadae48ba0782a0daf91fde3a36f19b4
leroy <leroy.kariuki@student.moringaschool.com>
2023-08-03 10:50:10 +0300

    CurrentGames.js

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import { fetchGames } from "./api";
+import { fetchGames } from "./Api";
 
 const App = () => {
   const [games, setGames] = useState({ currentGames: [], upcomingGames: [] });
--- game-app/src/UpcomingGames.js
+++ game-app/src/UpcomingGames.js
@@ -3,7 +3,10 @@ import GameList from "./GameList";
 
 const UpcomingGames = ({ upcomingGames }) => {
   return (
+    <div>
+      <h2>Upcoming Games</h2>
+      <GameList games={upcomingGames} />
+    </div>
   );
 };
-
 
